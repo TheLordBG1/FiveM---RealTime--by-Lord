deb17ca8f062e19a9369685cc57c2c943ca4efba
DonJoan <71422908+DonJoan@users.noreply.github.com>
2021-04-16 08:59:46 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,11 +1,10 @@
 ESX = nil
 
-Citizen.CreateThread(function ()
-  while ESX == nil do
-    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-    Citizen.Wait(0)
- 	PlayerData = ESX.GetPlayerData()
-  end
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
 end)
 
 local showTime = true -- Настройка за това дали да се показва времето автоматично при влизане. При false щес е показва само когато се използва командата.
