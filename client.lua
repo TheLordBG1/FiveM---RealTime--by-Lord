e8dafbfbb520d2c3136daf545e994103f2108cac
TheLordBG1 <60077295+TheLordBG1@users.noreply.github.com>
2021-04-16 06:28:56 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -3,6 +3,14 @@ ESX = nil
 local showTime = true -- Настройка за това дали да се показва времето автоматично при влизане. При false щес е показва само когато се използва командата.
 local Command = "time" -- Името на командата за показване на времето.
 print"=======Time script by Lord======="
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+end)
+
 RegisterCommand(Command, function(source, args, rawCommand)
     if showTime then 
         showTime = false
