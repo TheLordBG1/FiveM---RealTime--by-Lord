626c990d6b09cab73565f016416e88a68fc872cc
DonJoan <71422908+DonJoan@users.noreply.github.com>
2021-04-16 06:55:41 +0300

    Update and rename __resource.lua to fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+-- Resource Metadata
+fx_version 'cerulean'
+game 'gta5'
+
+author 'TheLordBG1'
+description 'Simple Real Time'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+ui_page('UI.html') 
+
+
+files({
+    'UI.html'
+})
